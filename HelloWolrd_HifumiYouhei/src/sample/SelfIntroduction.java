fcb385f0011c0d2dc2b2316bf791fcafcaca3060
seali <seali@DESKTOP-H6PB507>
2023-09-14 21:42:29 +0900

    java1-3新規作成

---
+++ HelloWolrd_HifumiYouhei/src/sample/SelfIntroduction.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// 氏名
+		String name = "一二三 陽平";
+		// 年齢
+		int age = 21;
+		// 趣味
+		String hobby = "なんかいろいろ";
+		
+		/*
+		* 自己紹介文を出力する処理
+		*/
+
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+	}
+
+}
