761bf3135ad8bac418dc062e61bab051853cf8c6
seali <seali@DESKTOP-H6PB507>
2023-10-03 22:54:55 +0900

    1.16 データ型とメモリの関係
    問５
    ・横列を020までになるよう、セットし直しました。

--- HelloWolrd_HifumiYouhei/src/curriculum_B/Qes5.java
+++ HelloWolrd_HifumiYouhei/src/curriculum_B/Qes5.java
@@ -1,27 +1,25 @@
 package curriculum_B;
 
 public class Qes5 {
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+    public static void main(String[] args) {
 
-		// 001から009までの掛け算を行うための外側のループ
-		for (int i = 1; i <= 9; i++) {
+        // 001から020までの掛け算を行うための外側のループ
+        for (int i = 1; i <= 9; i++) {
 
-			// 001から009までの掛け算を行うための内側のループ
-			for (int j = 1; j <= 9; j++) {
+            // 001から020までの掛け算を行うための内側のループ
+            for (int j = 1; j <= 20; j++) {
 
-				// 書式付きで掛け算の結果を表示。例: "001 × 001 = 001"
-				System.out.printf("%03d * %03d = %03d", j, i, i * j);
+                // 書式付きで掛け算の結果を表示。例: "001 × 001 = 001"
+                System.out.printf("%03d * %03d = %03d", j, i, i * j);
 
-				// jが9でない場合（最後の列でない場合）、区切り文字" || "を表示
-				if (j != 9) {
-					System.out.print(" || ");
-				}
-			}
+                // jが20でない場合（最後の列でない場合）、区切り文字" || "を表示
+                if (j != 20) {
+                    System.out.print(" || ");
+                }
+            }
 
-			// 一つの段が終わったら改行を入れて、次の段の表示に備える。
-			System.out.println();
-		}
-	}
-
+            // 一つの段が終わったら改行を入れて、次の段の表示に備える。
+            System.out.println();
+        }
+    }
 }
