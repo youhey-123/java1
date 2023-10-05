62744fee3954e49943badd30b6020060ca10831c
seali <seali@DESKTOP-H6PB507>
2023-10-06 00:26:15 +0900

    1.19 クラスの基本
    Dogクラス
    ・他クラスでも使えるようアクセス修飾子をpublicにしてDogオブジェクトを作成しました

---
+++ HelloWolrd_HifumiYouhei/src/Dog/Dog.java
@@ -0,0 +1,20 @@
+package Dog;
+
+public class Dog {
+	// Q1：動物の名前のフィールド変数
+	public String name;
+
+	// Q2：動物の数のフィールド変数
+	public int count;
+
+	// Q3：コンストラクタで動物の名前に「犬」を代入
+	public Dog() {
+		name = "犬";
+	}
+
+	// Q4：コンストラクタで動物の数を代入
+	public Dog(int number) {
+		name = "犬";
+		count = number;
+	}
+}
