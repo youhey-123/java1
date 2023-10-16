08df1e5ac1bc41574c7304303fd0402a243e4fa1
seali <seali@DESKTOP-H6PB507>
2023-10-17 00:05:16 +0900

    1.29 オブジェクト指向の基本②
    ・Comparatorインタフェースを用い、都道府県のソートが正しく実行されるようにしました。

--- 28_29_question_new/src/sort/SortLogic.java
+++ 28_29_question_new/src/sort/SortLogic.java
@@ -9,9 +9,8 @@ import prefecture.Prefecture;
 // このクラスは都道府県のリストを特定の基準でソートするためのロジックを提供します。
 public class SortLogic {
     
-    /**
+    /*
      * 選択された都道府県のインデックスに基づいて、都道府県を面積でソートするメソッド。
-     * 
      * @param prefectures 全都道府県のリスト
      * @param indices ソート対象となる都道府県のインデックスのリスト
      * @param ascending 昇順でソートする場合はtrue、降順の場合はfalse
@@ -19,14 +18,28 @@ public class SortLogic {
      */
     public static List<Prefecture> sortPrefecturesByArea(List<Prefecture> prefectures, List<Integer> indices, boolean ascending) {
         
+        // 面積を比較するためのComparatorを定義
+        // 匿名内部クラスを使用して昇順・降順の比較ロジックを定義
+        Comparator<Prefecture> comparator = new Comparator<Prefecture>() {
+           
+            public int compare(Prefecture p1, Prefecture p2) {
+                if (ascending) {
+                    // 昇順の場合は面積が小さい順にソート
+                    return Double.compare(p1.getArea(), p2.getArea());
+                } else {
+                    // 降順の場合は面積が大きい順にソート
+                    return Double.compare(p2.getArea(), p1.getArea());
+                }
+            }
+        };
+        
         // StreamAPIを使用して以下の処理を行う
         return indices.stream()
                       // indicesの各要素（都道府県のインデックス）に基づいて、実際の都道府県オブジェクトを取得する
                       .map(prefectures::get)
                       
-                      // 取得した都道府県オブジェクトを面積でソートする。
-                      // ここでは降順でソートされるが、ascendingがtrueの場合は後の処理で昇順に戻される。
-                      .sorted(Comparator.comparingDouble(Prefecture::getArea).reversed())
+                      // 定義したcomparatorを使用して都道府県をソート
+                      .sorted(comparator)
                       
                       // ソートされた都道府県のリストを生成する
                       .collect(Collectors.toList());
