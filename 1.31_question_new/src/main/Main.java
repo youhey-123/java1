7773cc700c580fd9454160def6cb7770116e6b90
seali <seali@DESKTOP-H6PB507>
2023-10-19 18:45:50 +0900

    1.31 自己紹介プログラムを作成②
    <修正点>
    ・ 問題3:countの値を返すゲッターメソッドを用い、Main.javaで
    System.out.printlnを使い「合計〇〇人です」と出力するよう修正しました。

--- 1.31_question_new/src/main/Main.java
+++ 1.31_question_new/src/main/Main.java
@@ -3,14 +3,17 @@ package main;
 import person.Person;
 
 public class Main {
-    public static void main(String[] argos) {
+    public static void main(String[] args) {
         Person person1 = new Person("鈴木太郎", 20, 1.7, 60);
         person1.print();
 
         Person person2 = new Person("山田花子", 22, 1.5, 40);
         person2.print();
         
-        // 問題6：クラスメソッド「printCount」を呼び出してください
+        // 問題3:System.out.printlnにゲッターメソッドを用い、合計人数を出力
+        System.out.println("合計" + Person.getCount() + "人です");
+        
+        // 問題6：クラスメソッド「printCount」を呼び出す
         Person.printCount();
     }
 }
